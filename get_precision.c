c6d756bd23d992257570bc42781d400866dadd56
Dani-1138 <danieldemeke92@gmail.com>
2023-01-23 05:54:45 -0800

    precision

--- get_precision.c
+++ get_precision.c
@@ -5,8 +5,10 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
+ *
  * Return: Precision.
  */
+
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
