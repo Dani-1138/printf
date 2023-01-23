0d287a9c17a5bc99a2a04fd39dec679109f308e3
Dani-1138 <danieldemeke92@gmail.com>
2023-01-23 04:56:50 -0800

    functions2

--- functions2.c
+++ functions2.c
@@ -9,7 +9,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed.
+ * Return: Number of chars printed in the program.
  */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
