a2d3a10a2bf3ec77bee21d772e3b8b7a6f37d975
Dani-1138 <danieldemeke92@gmail.com>
2023-01-23 04:53:18 -0800

    _printf

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
- * Return: Printed chars.
+ * Return: Printed chars return.
  */
 int _printf(const char *format, ...)
 {
