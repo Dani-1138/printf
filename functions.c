4179d495054b2305e1f83466e0406bc5ebd9e21e
Dani-1138 <danieldemeke92@gmail.com>
2023-01-23 04:57:44 -0800

    functions

--- functions.c
+++ functions.c
@@ -10,7 +10,7 @@
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: Number of chars printed.
  */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
